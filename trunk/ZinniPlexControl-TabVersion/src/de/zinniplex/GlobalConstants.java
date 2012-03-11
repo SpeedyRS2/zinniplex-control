13d1afdaeea12882c51cdb370787eeecca8dc3a1
andreas.zinnikus@gmail.com <andreas.zinnikus@gmail.com@72327b63-b40e-d3a3-6ac2-b9a5147c35e0>
2012-03-11 17:08:43 +0000

    löschen

--- trunk/ZinniPlexControl-TabVersion/src/de/zinniplex/GlobalConstants.java
+++
@@ -1,9 +0,0 @@
-package de.zinniplex;
-
-public interface GlobalConstants {
-
-	public static final String PREFS_ONKYO = "prefs_onkyo";
-	public static final String PREFS_LISTENING_MODE = "prefs_listeningMode";
-	public static final String PREFS_VOLUME = "prefs_volume";
-	public static final String PREFS_SOURCE = "prefs_source";
-}
