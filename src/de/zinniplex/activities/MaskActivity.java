99dd28186074996dec423b1bb1bab69b4797758b
andreas.zinnikus@gmail.com <andreas.zinnikus@gmail.com@72327b63-b40e-d3a3-6ac2-b9a5147c35e0>
2012-03-31 12:44:41 +0000

    Maskierung fähr so lange, wie Taste gedrückt wird.

--- src/de/zinniplex/activities/MaskActivity.java
+++ src/de/zinniplex/activities/MaskActivity.java
@@ -1,9 +1,13 @@
 package de.zinniplex.activities;
 
-import de.zinniplex.R;
-import de.zinniplex.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import de.zinniplex.R;
+import de.zinniplex.eventghost.EventGhostCommands;
+import de.zinniplex.http.SubmitCommandAsyncTask;
 
 public class MaskActivity extends Activity {
 	@Override
@@ -11,5 +15,34 @@ public class MaskActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mask_layout);
 
+		// Maskierung rauf fahren, solange gedrueckt wird
+		Button maskupButton = (Button) this.findViewById(R.id.maskopen);
+		maskupButton.setOnTouchListener(new View.OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					new SubmitCommandAsyncTask().execute(EventGhostCommands.COMMAND_MASK_OPEN_PRESSED);
+				} else if (event.getAction() == MotionEvent.ACTION_UP) {
+					new SubmitCommandAsyncTask().execute(EventGhostCommands.COMMAND_MASK_OPEN_RELEASED);
+				}
+				return false;
+			}
+		});
+
+		// Maskierung runter fahren, solange gedrueckt wird
+		Button maskdownButton = (Button) this.findViewById(R.id.maskclose);
+		maskdownButton.setOnTouchListener(new View.OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					new SubmitCommandAsyncTask().execute(EventGhostCommands.COMMAND_MASK_CLOSE_PRESSED);
+				} else if (event.getAction() == MotionEvent.ACTION_UP) {
+					new SubmitCommandAsyncTask().execute(EventGhostCommands.COMMAND_MASK_CLOSE_RELEASED);
+				}
+				return false;
+			}
+		});
 	}
 }
